b547827da4f2e0ea6fb04434025612fe1493aac6
GabrielFPSouza <gabriel.f.p.souza@hotmail.com>
2017-09-18 02:01:34 -0300

    Quarto Commit

---
+++ APP/src/main/java/br/luizalabs/controller/ProdutoController.java
@@ -0,0 +1,20 @@
+package br.luizalabs.controller;
+
+import br.luizalabs.model.Produto;
+
+import java.util.ArrayList;
+
+/**
+ * Classe responsável por conter os métodos de manipulação de resultado
+ *
+ * @author Gabriel.Souza
+ * @since 17/09/2017 23:30
+ * @version 1.0
+ */
+public class ProdutoController {
+
+    public ArrayList<Produto> listar(){
+        return new ArrayList<Produto>();
+    }
+
+}
--- APP/src/main/java/br/luizalabs/model/Produto.java
+++ APP/src/main/java/br/luizalabs/model/Produto.java
@@ -1,7 +1,79 @@
 package br.luizalabs.model;
 
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.Currency;
+import java.util.HashMap;
+
 /**
  * Classe responsável por conter os atributos do objeto Produto
- */
+ *
+ * @author Gabriel.Souza
+ * @since 17/09/2017
+ * @version 1.0
+*/
+@XmlRootElement
 public class Produto {
+
+    private String id;
+    private String ean;
+    private String title;
+    private String brand;
+    private Currency price;
+    private Integer stock;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getEan() {
+        return ean;
+    }
+
+    public void setEan(String ean) {
+        this.ean = ean;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public Currency getPrice() {
+        return price;
+    }
+
+    public void setPrice(Currency price) {
+        this.price = price;
+    }
+
+    public Integer getStock() {
+        return stock;
+    }
+
+    public void setStock(Integer stock) {
+        this.stock = stock;
+    }
+
+    @Override
+    public String toString(){
+        return "Produto [id=" + id + ", ean=" + ean + ", title=" + title + ", brand=" + brand
+                + ", price=" + price + ", stock=" + stock + "]";
+    }
+
+
 }
